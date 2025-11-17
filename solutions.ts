42df6c6e67c67ae9d4b3598e5ed021627506f67a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-11-17 21:12:06 +0600

    second and third problem solved

--- solutions.ts
+++ solutions.ts
@@ -5,9 +5,39 @@ const formatValue = (value: FormatValueTypes): FormatValueTypes => {
         return value * 10;
     } else if (typeof value === "string") {
         return value.toUpperCase();
-    } else if (typeof value === "boolean") {
-        return !value;
     } else {
-        return false;
+        return !value;
     }
+}
+
+const getLength = (value: string | any[]): number => {
+    if (typeof value === "string") {
+        return value.length;
+    }
+    if (Array.isArray(value)) {
+        return value.length;
+    }
+    throw new Error("invalid type");
+}
+
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+interface Product {
+    title: string;
+    rating: number;
+}
+
+const filterByRating = (products: Product[]): Product[] => {
+    const selectedProducts = products.filter(product => product.rating >= 4);
+    return selectedProducts;
 }
